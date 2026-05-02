a82ae1f4d75e10502465436e2ad32480506d673d
V171998 <tarade44@gmail.com>
2026-05-02 07:09:56 +0530

    created react app with vite & added tailwindcss config

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 class="text-3xl font-bold underline text-blue-500">hello world!!</h1>
+
+    
+    </>
+  )
+}
+
+export default App
