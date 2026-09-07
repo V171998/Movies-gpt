5c41a0203779442dbe1dcdce94a5ab0100b79f62
V171998 <tarade44@gmail.com>
2026-09-07 23:14:20 +0530

    created a custom hook to fetch the trailer and store it in the store

--- src/component/VideoBackground.jsx
+++ src/component/VideoBackground.jsx
@@ -3,36 +3,13 @@ import React, { useEffect } from 'react'
 import { API_OPTIONS } from '../utils/constant';
 import { useDispatch, useSelector } from 'react-redux';
 import { addtrailerVideo } from "../utils/movieSlice";
+import useMovieTrailer from '../hooks/useMovieTrailer';
 
 const VideoBackground = ({ movieId }) => {
     
     const trailerVideo = useSelector(store => store.movies?.trailerVideo)
-    const dispatch = useDispatch();
-
-    const getMovieVideos = async () => {
-        
-        const data = await fetch(
-            "https://api.themoviedb.org/3/movie/969681/videos?language=en-US",
-            API_OPTIONS,
-        );
-
-        const json = await data.json();
-        console.log(json);
-
-
-        const filterData = json.results.filter(
-            (video) => video.type === "Trailer"
-        );
-        const trailer = filterData.length ? filterData[0] : json.results[0];
-        console.log(trailer);
-        dispatch(addtrailerVideo(trailer));
-
-
-    }
-
-    useEffect(() => {
-        getMovieVideos();
-    }, []);
+    
+    useMovieTrailer(movieId);
 
     return (
       <div>
